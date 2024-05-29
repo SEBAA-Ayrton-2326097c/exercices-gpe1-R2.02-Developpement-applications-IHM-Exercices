94be939687be22aa1ce094e8d8f9b0c332caa2be
SEBAA Ayrton <157604336+SEBAA-Ayrton-2326097c@users.noreply.github.com>
2024-05-29 17:30:20 +0200

    Update MainPersonnes.java

--- src/main/java/fr/amu/iut/exercice14/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice14/MainPersonnes.java
@@ -1,45 +1,96 @@
-package fr.amu.iut.exercice4;
+package fr.amu.iut.exercice14;
 
+import javafx.beans.Observable;
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 
+
+
 @SuppressWarnings("Duplicates")
 public class MainPersonnes {
+
+
+
 
     private static SimpleListProperty<Personne> lesPersonnes;
     private static IntegerProperty ageMoyen;
     private static IntegerProperty nbParisiens;
-
     private static IntegerBinding calculAgeMoyen;
     private static IntegerBinding calculnbParisiens;
 
     public static void main(String[] args) {
 
-        lesPersonnes = new SimpleListProperty<>(FXCollections.observableArrayList());
+        lesPersonnes = new SimpleListProperty<>(FXCollections.observableArrayList(personne -> new Observable[]{
+                personne.getAge(), personne.getVilleDeNaissance()
+        }));
         ageMoyen = new SimpleIntegerProperty(0);
+        nbParisiens = new SimpleIntegerProperty(0);
 
+
+
+        calculnbParisiens = new IntegerBinding() {
+            {
+                this.bind(lesPersonnes);
+            }
+            protected int computeValue() {
+                int cmp = 0;
+                if (lesPersonnes.getSize() == 0) {
+                    return 0;
+                }
+                for (Personne p : lesPersonnes) {
+                    if (p.getVilleDeNaissance().get()=="Paris") ++ cmp;
+                }
+                return cmp;
+            }
+        };
+
+
+        nbParisiens.bind(calculnbParisiens);
+        calculAgeMoyen = new IntegerBinding() {
+            {
+                this.bind(lesPersonnes);
+            }
+
+
+
+            @Override
+            protected int computeValue() {
+                int moy = 0;
+                int cmp = 0;
+                if (lesPersonnes.getSize() == 0) {
+                    return 0;
+                }
+                for (Personne p : lesPersonnes) {
+                    moy += p.getAge().get();
+                    ++cmp;
+                }
+                return moy/cmp;
+            }
+        };
+        ageMoyen.bind(calculAgeMoyen);
         question1();
-//        question2();
+        question2();
     }
 
+
     public static void question1() {
-        System.out.println("1 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("1 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
         Personne pierre = new Personne("Pierre", 20);
         lesPersonnes.add(pierre);
-        System.out.println("2 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("2 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
         Personne paul = new Personne("Paul", 40);
         lesPersonnes.add(paul);
-        System.out.println("3 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("3 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
         Personne jacques = new Personne("Jacques", 60);
         lesPersonnes.add(jacques);
-        System.out.println("4 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("4 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
         paul.setAge(100);
-        System.out.println("5 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("5 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
         lesPersonnes.remove(paul);
-        System.out.println("6 - L'age moyen est de " + ageMoyen.getValue() + " ans");
+        System.out.println("6 - L'âge moyen est de " + ageMoyen.getValue() + " ans");
     }
 
     public static void question2() {
@@ -54,4 +105,3 @@ public class MainPersonnes {
     }
 
 }
-
